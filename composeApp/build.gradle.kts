e01c9e17195c4e3c18770257a6fb51b1a6fc7701
cygao <434260666@qq.com>
2025-11-21 15:01:57 +0800

    Initial commit

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,194 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.kotlinxSerialization)
+    alias(libs.plugins.skie)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
+    id("io.github.ttypic.swiftklib") version "0.6.4"
+}
+
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+    
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+    
+    jvm()
+    
+//    js {
+//        browser()
+//        binaries.executable()
+//    }
+    
+//    @OptIn(ExperimentalWasmDsl::class)
+//    wasmJs {
+//        browser()
+//        binaries.executable()
+//    }
+    
+    sourceSets {
+        androidMain.dependencies {
+            implementation(compose.preview)
+            implementation(libs.androidx.activity.compose)
+        }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodelCompose)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
+
+            implementation(libs.skie.annotations)
+            implementation(libs.androidx.room.runtime)
+            implementation(libs.sqlite.bundled)
+            implementation(libs.kermit)
+
+            // DateTime
+            implementation(libs.kotlinx.datetime)
+
+            // Kodein for dependency injection
+            implementation(libs.kodein.di)
+            implementation(libs.kodein.di.framework.compose)
+
+            // https://developer.android.google.cn/kotlin/multiplatform/room
+            // https://github.com/android/kotlin-multiplatform-samples/blob/main/Fruitties/shared/build.gradle.kts
+
+            // Calendar
+            implementation("com.kizitonwose.calendar:compose-multiplatform:2.8.0")
+
+            // Cupertino
+            implementation(libs.cupertino)
+
+            implementation("org.jetbrains.androidx.navigation:navigation-compose:2.9.0-beta04")
+
+            // Jetpack DataStore
+            implementation(libs.androidx.datastore.core.okio)
+            implementation(libs.androidx.datastore.preferences.core)
+            implementation(libs.okio)
+            implementation(libs.sonner)
+
+            // Kotlin序列化
+            implementation(libs.kotlinx.serialization.json)
+
+            // Ktor网络请求
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.client.auth)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.ktor.client.logging)
+            implementation(libs.ktor.client.resources)
+
+            // Coil
+            implementation(libs.coil.compose)
+            implementation(libs.coil.network.ktor3)
+
+            implementation(libs.filekit.dialogs.compose)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+        jvmMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation(libs.kotlinx.coroutinesSwing)
+        }
+    }
+}
+
+android {
+    namespace = "com.kmpstudy"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.kmpstudy"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    debugImplementation(compose.uiTooling)
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.kmpstudy.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "com.kmpstudy"
+            packageVersion = "1.0.0"
+        }
+    }
+}
+
+
+// Room 配置
+room {
+    schemaDirectory("$projectDir/schemas")
+}
+
+// KSP 依赖配置
+dependencies {
+    add("kspCommonMainMetadata", libs.androidx.room.compiler)
+    add("kspAndroid", libs.androidx.room.compiler)
+//    add("kspIosX64", libs.androidx.room.compiler)
+//    add("kspIosArm64", libs.androidx.room.compiler)
+//    add("kspIosSimulatorArm64", libs.androidx.room.compiler)
+    add("kspJvm", libs.androidx.room.compiler)
+}
+
+skie {
+    features {
+        // https://skie.touchlab.co/features/flows-in-swiftui
+        enableSwiftUIObservingPreview = true
+    }
+
+    analytics {
+        enabled.set(false)
+    }
+}
+
+swiftklib {
+    create("SwiftLib") {
+        path = file("swift")
+        packageName("com.kmpstudy.objclibs.widgetbridge")
+    }
+}
---
+++ composeApp/src/jsMain/kotlin/com/kmpstudy/Platform.js.kt
@@ -0,0 +1,7 @@
+package com.kmpstudy
+
+class JsPlatform: Platform {
+    override val name: String = "Web with Kotlin/JS"
+}
+
+actual fun getPlatform(): Platform = JsPlatform()
